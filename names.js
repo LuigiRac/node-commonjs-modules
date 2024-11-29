8db364ffc78e853fef2ecfe361fe9146f3b90262
LuigiRac <lui.rac06@gmail.com>
2024-11-29 16:30:33 +0100

    push_prova_terminal

---
+++ names.js
@@ -0,0 +1,8 @@
+/* 1 - names.js
+1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+2. Esporta la funzione dal file. */
+
+// function fullName(firstName, lastName) {
+//     return { firstName: firstName, lastName: lastName }
+// }
+// console.log(names.fullName("Luis", "Rasera"));
